0fb8e9315f5c290d014d906c4505ce7aaec216c3
roefwin <regis-971@hotmail.coml>
2023-09-21 17:21:31 +0200

    create okhttpClient + add it authenticator and interceptor

--- app/src/main/java/com/example/newblablacar/tripseach/di/TripSearchDataModule.kt
+++ app/src/main/java/com/example/newblablacar/tripseach/di/TripSearchDataModule.kt
@@ -1,27 +1,40 @@
 package com.example.newblablacar.tripseach.di
 
+import com.example.newblablacar.tripseach.data.authenticator.TripSearchAuthenticator
+import com.example.newblablacar.tripseach.data.interceptor.TripSearchHeaderInterceptor
 import com.example.newblablacar.tripseach.data.remote.TripSearchApi
 import com.example.newblablacar.tripseach.utils.TRIP_SEARCH_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object TripSearchApiModule {
+object TripSearchDataModule {
 
     @Singleton
     @Provides
-    fun provideTripSearchApi(): TripSearchApi {
+    fun provideTripSearchApi(okHttpClient: OkHttpClient): TripSearchApi {
         val retrofit = Retrofit.Builder()
             .baseUrl(TRIP_SEARCH_URL)
+            .client(okHttpClient)
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
-
         return retrofit.create(TripSearchApi::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun provideOkHttp(
+        tripSearchHeaderInterceptor: TripSearchHeaderInterceptor,
+        tripSearchAuthenticator: TripSearchAuthenticator
+    ) = OkHttpClient.Builder()
+        .addInterceptor(tripSearchHeaderInterceptor)
+        .authenticator(tripSearchAuthenticator)
+        .build()
 }

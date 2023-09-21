ab225f1ce007e57845996b973d57e8619b20249d
roefwin <regis-971@hotmail.coml>
2023-09-21 15:56:10 +0200

    read sharedPref and refresh token in activity
    Testing step

--- app/src/main/java/com/example/newblablacar/MainActivity.kt
+++ app/src/main/java/com/example/newblablacar/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.newblablacar
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -13,8 +14,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import com.example.newblablacar.authentication.domain.AuthenticationRepository
+import com.example.newblablacar.authentication.utils.ACCESS_TOKEN_KEY
 import com.example.newblablacar.ui.theme.NewBlablacarTheme
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.internal.InjectedFieldSignature
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,6 +26,10 @@ class MainActivity : ComponentActivity() {
 
     @Inject
     lateinit var authenticationRepository: AuthenticationRepository
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,7 +38,9 @@ class MainActivity : ComponentActivity() {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     lifecycleScope.launch {
                         val token = authenticationRepository.refreshToken()
-                        Toast.makeText(this@MainActivity, token.toString(), Toast.LENGTH_LONG).show()
+                        val accessToken = sharedPreferences.getString(ACCESS_TOKEN_KEY, "no token available")
+                        Toast.makeText(this@MainActivity, token.javaClass.name, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@MainActivity, accessToken, Toast.LENGTH_LONG).show()
                     }
                 }
             }

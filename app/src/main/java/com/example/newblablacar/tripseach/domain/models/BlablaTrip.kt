67dcd81822c86f9057e856c49b839c60c793c470
roefwin <regis-971@hotmail.coml>
2023-09-21 16:29:15 +0200

    create Domain models for abstraction

---
+++ app/src/main/java/com/example/newblablacar/tripseach/domain/models/BlablaTrip.kt
@@ -0,0 +1,8 @@
+package com.example.newblablacar.tripseach.domain.models
+
+data class BlablaTrip(
+    val from :String = "no trip",
+    val to :String = "no trip",
+    val driverName : String = "no name",
+    val price : String = "no price"
+)

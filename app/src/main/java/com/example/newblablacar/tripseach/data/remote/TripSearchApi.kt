9b863ae9f763d0294140c3561f320536ab1700c1
roefwin <regis-971@hotmail.coml>
2023-09-21 18:36:43 +0200

    remove TripSearchParam need only Query

--- app/src/main/java/com/example/newblablacar/tripseach/data/remote/TripSearchApi.kt
+++ app/src/main/java/com/example/newblablacar/tripseach/data/remote/TripSearchApi.kt
@@ -1,11 +1,15 @@
 package com.example.newblablacar.tripseach.data.remote
 
-import com.example.newblablacar.tripseach.data.models.TripSearchParams
 import okhttp3.ResponseBody
-import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface TripSearchApi {
-    @GET("search")
-    suspend fun tripSearch(@Body tripSearchParams: TripSearchParams): ResponseBody
+    @GET("search/v7")
+    suspend fun tripSearch(
+        @Query("from_address") fromAddress: String,
+        @Query("to_address") toAddress: String,
+        @Query("search_uuid") searchUuid: String,
+        @Query("from_cursor") fromCursor: String?
+    ): ResponseBody
 }

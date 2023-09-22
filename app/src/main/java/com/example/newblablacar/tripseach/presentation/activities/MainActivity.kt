689cf4ced6df7f21f3b260225bba3beec538d53e
roefwin <regis-971@hotmail.coml>
2023-09-22 16:09:20 +0200

    adjust main activity launcher and collect state

--- app/src/main/java/com/example/newblablacar/tripseach/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/newblablacar/tripseach/presentation/activities/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.newblablacar.tripseach.presentation.activities
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -9,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,30 +20,44 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.newblablacar.tripseach.presentation.screens.address.BlablaTripAddressScreen
 import com.example.newblablacar.tripseach.presentation.screens.trips.BlablaTripListScreen
+import com.example.newblablacar.tripseach.presentation.screens.trips.BlablaTripListState
 import com.example.newblablacar.tripseach.presentation.screens.trips.BlablaTripListViewModel
 import com.example.newblablacar.ui.theme.NewBlablacarTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NewBlablacarTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    val viewModel: BlablaTripListViewModel by viewModels()
-                    val state = viewModel.state.collectAsState()
                     val navController = rememberNavController()
                     NavHost(navController = navController, startDestination = "tripaddress") {
                         composable("tripaddress") {
                             BlablaTripAddressScreen(navController)
                         }
                         composable("triplist") {
-                            BlablaTripListScreen(
-                                blablaTripListState = state.value
-                            )
+                            val viewModel: BlablaTripListViewModel by viewModels()
+                            val state = viewModel.state.collectAsState()
+                            LaunchedEffect(key1 = state) {
+                                if (state.value.isFailure) {
+                                    state.value.onFailure {
+                                        Toast.makeText(this@MainActivity, it.message, Toast.LENGTH_LONG).show()
+                                    }
+                                }
+                            }
+                            if (state.value.isSuccess) {
+                                BlablaTripListScreen(
+                                    blablaTripListState = state.value.getOrDefault(BlablaTripListState())
+                                )
+                            }
+
                         }
                     }
                 }

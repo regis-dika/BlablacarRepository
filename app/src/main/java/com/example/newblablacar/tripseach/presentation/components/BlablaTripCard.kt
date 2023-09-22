9b61a1c6b9b76093f569ab3ffd411eea12e7d526
roefwin <regis-971@hotmail.coml>
2023-09-22 17:31:28 +0200

    adjustement for the card

--- app/src/main/java/com/example/newblablacar/tripseach/presentation/components/BlablaTripCard.kt
+++ app/src/main/java/com/example/newblablacar/tripseach/presentation/components/BlablaTripCard.kt
@@ -1,6 +1,7 @@
 package com.example.newblablacar.tripseach.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,7 +10,11 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.newblablacar.tripseach.domain.models.BlablaTrip
@@ -23,15 +28,21 @@ fun BlablaTripCard(
     Card(modifier = modifier
         .fillMaxWidth()
         .padding(16.dp), onClick = { /*TODO*/ }) {
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp), horizontalArrangement = Arrangement.SpaceAround
+        ) {
             Column(verticalArrangement = Arrangement.SpaceAround) {
                 Text(text = "Départ : ${blablaTrip.from}")
                 Text(text = "Arrivée : ${blablaTrip.to}")
                 Row {
-                    Text(text = "Prénom : ${blablaTrip.driverName}")
+                    Text(text = "Prénom : ${blablaTrip.driverName}", fontWeight = FontWeight.SemiBold)
                 }
             }
-            Text(text = blablaTrip.price, modifier = Modifier.weight(1f))
+            Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.CenterEnd) {
+                Text(text = blablaTrip.price, fontFamily = FontFamily.Serif, fontWeight = FontWeight.Bold)
+            }
         }
     }
 }

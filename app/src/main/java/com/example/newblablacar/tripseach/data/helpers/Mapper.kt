26b47b85fe30a1b9d2396cf2e218abbf1efb7de8
roefwin <regis-971@hotmail.coml>
2023-09-22 17:20:33 +0200

    mapper implementation

--- app/src/main/java/com/example/newblablacar/tripseach/data/helpers/Mapper.kt
+++ app/src/main/java/com/example/newblablacar/tripseach/data/helpers/Mapper.kt
@@ -1,1 +1,16 @@
 package com.example.newblablacar.tripseach.data.helpers
+
+import com.example.newblablacar.tripseach.data.models.BlalaTripsDto
+import com.example.newblablacar.tripseach.domain.models.BlablaTrip
+
+fun BlalaTripsDto.toBlalaTripDomain(): List<BlablaTrip> {
+    val trips = this.trips
+    return trips.map {
+        BlablaTrip(
+            it.waypoints.get(0).mainText,
+            it.waypoints.get(1).mainText,
+            it.driver.displayName,
+            it.priceDetails.price
+        )
+    }
+}

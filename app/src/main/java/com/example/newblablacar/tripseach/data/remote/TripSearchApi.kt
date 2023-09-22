f2abbb5748dbf1b2deb97c6ecc81a4c2ad8aa52e
roefwin <regis-971@hotmail.coml>
2023-09-22 15:36:42 +0200

    update viewModel

--- app/src/main/java/com/example/newblablacar/tripseach/data/remote/TripSearchApi.kt
+++ app/src/main/java/com/example/newblablacar/tripseach/data/remote/TripSearchApi.kt
@@ -1,5 +1,6 @@
 package com.example.newblablacar.tripseach.data.remote
 
+import com.example.newblablacar.tripseach.data.models.BlalaTripsDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,5 +12,5 @@ interface TripSearchApi {
         @Query("to_address") toAddress: String,
         @Query("search_uuid") searchUuid: String,
         @Query("from_cursor") fromCursor: String?
-    ): ResponseBody
+    ): BlalaTripsDto
 }
--- app/src/main/java/com/example/newblablacar/tripseach/presentation/screens/trips/BlablaTripListViewModel.kt
+++ app/src/main/java/com/example/newblablacar/tripseach/presentation/screens/trips/BlablaTripListViewModel.kt
@@ -1,26 +1,33 @@
 package com.example.newblablacar.tripseach.presentation.screens.trips
 
 import androidx.lifecycle.ViewModel
-import com.example.newblablacar.tripseach.domain.models.BlablaTrip
+import androidx.lifecycle.viewModelScope
+import com.example.newblablacar.core.tripsearch.usescase.GetTripsSearchUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class BlablaTripListViewModel @Inject constructor() : ViewModel() {
-    val state: MutableStateFlow<BlablaTripListState> = MutableStateFlow(
-        BlablaTripListState(
-            listOf(
-                BlablaTrip(),
-                BlablaTrip(),
-                BlablaTrip(),
-                BlablaTrip(),
-                BlablaTrip(),
-                BlablaTrip(),
-                BlablaTrip(),
-                BlablaTrip(),
-                BlablaTrip(),
+class BlablaTripListViewModel @Inject constructor(
+    private val getTripsSearchUseCase: GetTripsSearchUseCase
+) : ViewModel() {
+    val state: MutableStateFlow<Result<BlablaTripListState>> = MutableStateFlow(
+        Result.success(
+            BlablaTripListState(
+                listOf()
             )
         )
     )
+
+    fun fetchTrip(searchUuid: String) {
+        viewModelScope.launch {
+            val result = getTripsSearchUseCase.invoke("Paris", "Toulouse", "8401658C-E98D-457E-A087-34FA2D979D694R", null)
+            if (result.isSuccess) {
+                state.value = Result.success(BlablaTripListState(result.getOrDefault(listOf())))
+            } else {
+                state.value = Result.failure(result.exceptionOrNull() ?: Throwable("error"))
+            }
+        }
+    }
 }
